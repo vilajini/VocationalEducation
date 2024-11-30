cae6f21b2a3714fdca48c1fddd639c7c9e052150
vilajini <75477877+vilajini@users.noreply.github.com>
2024-11-30 18:03:44 +0530

    Delete viva directory

--- viva/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('generate-question/', views.generate_question, name='generate_question'),
-    path('submit-answer/', views.submit_answer, name='submit_answer'),
-
-]
